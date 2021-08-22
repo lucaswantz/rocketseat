429610d16c4258a333a479ebd52e4e51bc9be4f1
Lucas Wantz da Motta <lucaswantz@hotmail.com>
2021-08-22 20:13:05 -0300

    Checkout de pagamento

--- ignite/reactjs/ignews/src/components/SubscribeButton/index.tsx
+++ ignite/reactjs/ignews/src/components/SubscribeButton/index.tsx
@@ -1,23 +1,29 @@
 import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
 	priceId: string;
 }
 
-// getServerSideProps (SSR)
-//
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 	const [session] = useSession();
 
-	function handleSubscribe() {
+	async function handleSubscribe() {
 		if (!session) {
 			signIn('github');
 			return;
 		}
 
-		// criacao da chekcout session
-
+		try {
+			const response = await api.post('/subscribe');
+			const { sessionId } = response.data;
+			const stripe = await getStripeJs();
+			await stripe.redirectToCheckout({ sessionId });
+		} catch (error) {
+			alert(error.message);
+		}
 	}
 
 	return (
