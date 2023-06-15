da4d423782953da05b886be59f935e001c95f874
Lucas W. da Motta <lucaswantz@hotmail.com>
2023-06-14 22:52:44 -0300

    Add eslint to project

--- ignite/reactjs/02-ignite-timer/src/App.tsx
+++ ignite/reactjs/02-ignite-timer/src/App.tsx
@@ -1,18 +1,19 @@
-import { ThemeProvider } from 'styled-components';
-import { Button } from "./components/Button";
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
 
-export function App() 	{
-	return (
-		<ThemeProvider theme={defaultTheme}>
-			<Button variant="primary" />
-			<Button variant="secondary" />
-			<Button variant="success" />
-			<Button variant="danger" />
-			<Button />
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
-			<GlobalStyle />
-		</ThemeProvider>
-	)
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
